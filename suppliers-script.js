d640947e11b79fdcc481591a214917dcf938c6ca
Rolf Hammerstad <rolfh@users.noreply.github.com>
2020-04-11 11:17:31 +0200

    VVS Eksperten, suppliers script prepared for API

--- suppliers-script.js
+++ suppliers-script.js
@@ -1,85 +1,95 @@
 var suppliersVm = new Vue({
 	el: '#suppliers-app',
-	data: function() {
+	data: function () {
 		return {
 			suppliers: suppliers,
 			activeCategory: suppliers[0].categoryName,
 			search: '',
-			listType: 'cards'
+			listType: 'cards',
 		}
 	},
-	mounted: function() {
+	mounted: function () {
 		this.setKeys()
 		this.combineSuppliersInAll()
+
+		/* 
+		
+			Fjern kommentering her etter bildene ikke er så store
+			Fjern også koden over i denne funksjonen: this.setKeys() og this.combineSuppliersInAll()
+
+		*/
+		// var that = this
+		// $.getJSON('/forhandlerkategorier.json', function (data) {
+		// 	that.suppliers = data
+		// 	that.setKeys()
+		// 	that.combineSuppliersInAll()
+		// })
 	},
 	methods: {
-		setKeys: function() {
-			this.suppliers = this.suppliers.map(function(category) {
-				category.items = category.items.map(function(supplier) {
+		setKeys: function () {
+			this.suppliers = this.suppliers.map(function (category) {
+				category.items = category.items.map(function (supplier) {
 					supplier.key =
-						'key' +
-						Math.random()
-							.toString(36)
-							.substr(2, 9)
+						'key' + Math.random().toString(36).substr(2, 9)
 					return supplier
 				})
 				return category
 			})
 		},
-		combineSuppliersInAll: function() {
-			this.suppliers[0].items = _.uniqWith(this.allSuppliers, function(
+		combineSuppliersInAll: function () {
+			this.suppliers[0].items = _.uniqWith(this.allSuppliers, function (
 				arrVal,
 				othVal
 			) {
 				return arrVal.name == othVal.name
 			})
-		}
+		},
 	},
 	computed: {
-		allSuppliers: function() {
+		allSuppliers: function () {
 			return this.suppliers
-				.reduce(function(accumulator, category) {
+				.reduce(function (accumulator, category) {
 					return $.merge(accumulator, category.items)
 				}, [])
 				.slice()
-				.sort(function(a, b) {
+				.sort(function (a, b) {
 					return a.name > b.name ? 1 : -1
 				})
 		},
-		activeSuppliers: function() {
+		activeSuppliers: function () {
 			var that = this
 			if (this.search != '') {
-				var all = _.uniqWith(this.allSuppliers, function(
+				var all = _.uniqWith(this.allSuppliers, function (
 					arrVal,
 					othVal
 				) {
 					return arrVal.name == othVal.name
 				})
 				return all
-					.filter(function(element) {
+					.filter(function (element) {
 						return _.includes(
 							element.name.toLowerCase(),
 							that.search.toLowerCase()
 						)
 					})
 					.slice()
-					.sort(function(a, b) {
+					.sort(function (a, b) {
 						return a.name > b.name ? 1 : -1
 					})
 			}
 			return this.suppliers
-				.filter(function(supplier) {
+				.filter(function (supplier) {
 					return supplier.categoryName == that.activeCategory
 				})[0]
 				.items.slice()
-				.sort(function(a, b) {
+				.sort(function (a, b) {
 					return a.name > b.name ? 1 : -1
 				})
 		},
-		suppliersAmount: function() {
-			return this.suppliers.reduce(function(accumulator, supplier) {
+		suppliersAmount: function () {
+			return this.suppliers.reduce(function (accumulator, supplier) {
 				return accumulator + supplier.items.length
 			}, 0)
-		}
-	}
+		},
+	},
 })
